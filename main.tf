07c9ab679c0a55c2744356c555c3ab0603ff8c4b
Hyndavi <venkatahyndavi@gmail.com>
2023-11-07 15:19:24 +0530

    push files to github

--- main.tf
+++ main.tf
@@ -48,14 +48,14 @@ resource "aws_api_gateway_rest_api" "example" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "cognito"
 }
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
   resource_id   = "${aws_api_gateway_resource.proxy.id}"
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = "COGNITO_USER_POOLS"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -66,6 +66,15 @@ resource "aws_api_gateway_integration" "lambda" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.example.invoke_arn}"
+}
+
+resource "aws_api_gateway_authorizer" "lambda" {
+  name                   = "example-lambda-authorizer"
+  rest_api_id            = aws_api_gateway_rest_api.example.id
+  type                   = "TOKEN"
+  identity_source        = "method.request.header.Authorization"
+  authorizer_uri         = aws_lambda_function.example.invoke_arn
+  authorizer_credentials = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -106,11 +115,36 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
 }
 
+resource "aws_cognito_user_pool" "example" {
+  name = "example-user-pool"
+  # Configure other user pool attributes as needed
+}
+
+resource "aws_cognito_user_pool_client" "example" {
+  name                     = "example-user-pool-client"
+  user_pool_id             = aws_cognito_user_pool.example.id
+  generate_secret          = true
+  explicit_auth_flows       = ["ALLOW_REFRESH_TOKEN_AUTH"]
+}
+
+resource "aws_cognito_user_pool_domain" "example" {
+  domain = "example-domain"
+  user_pool_id = aws_cognito_user_pool.example.id
+}
+
+resource "aws_api_gateway_authorizer" "cognito" {
+  name                   = "example-cognito-authorizer"
+  rest_api_id            = aws_api_gateway_rest_api.example.id
+  type                   = "COGNITO_USER_POOLS"
+  identity_source        = "method.request.header.Authorization"
+  provider_arns          = [aws_cognito_user_pool.example.arn]
+}
+
 terraform {
   backend "s3" {
-    bucket = "terraformstatfile"  # Replace with your bucket name
+    bucket = "terraformstatefile0"  # Replace with your bucket name
     key    = "terraform.tfstate"
-    region = "us-east-1"  # Replace with your preferred region
+    region = "ap-south-1"  # Replace with your preferred region
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "aws_region" {
   description = "The AWS region where the resources will be provisioned."
-  default = "us-east-1"  # Replace this with your desired AWS region
+  default = "ap-south-1"  # Replace this with your desired AWS region
 }
